41562f78839451afa1251948a567485bbc2f5e0d
jjansa <jpjansa@gmail.com>
2019-07-06 22:13:42 -0500

    implemented form processing and data passing from controller to view.

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -3,15 +3,37 @@ package org.launchcode.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
+    static  ArrayList<String> cheeses = new ArrayList<>();
+
     //Request path: /cheese
     @RequestMapping("")
     public String index(Model model){
+
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
         return  "cheese/index";
+    }
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model){
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName){
+        cheeses.add(cheeseName);
+        // Redirect to /cheese
+        return  "redirect:";
+
+
     }
 }
