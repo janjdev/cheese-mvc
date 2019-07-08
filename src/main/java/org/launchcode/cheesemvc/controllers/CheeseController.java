dc8c847b38f1939d344883ebd4e83b2bed64a708
jjansa <jpjansa@gmail.com>
2019-07-07 21:04:37 -0500

    converted hashmap to array list and updated views.

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,18 +1,21 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static  ArrayList<String> cheeses = new ArrayList<>();
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
     //Request path: /cheese
     @RequestMapping("")
@@ -29,11 +32,27 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String description){
+        Cheese newCheese = new Cheese(cheeseName, description);
+        cheeses.add(newCheese);
         // Redirect to /cheese
         return  "redirect:";
-
-
     }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public  String displayRemoveCheeseForm(Model model){
+        model.addAttribute("cheeses", cheeses );
+        model.addAttribute("title", "Remove Cheese");
+        return  "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST )
+    public String processRemoveCheese(@RequestParam ArrayList<String> cheese){
+       for (String aCheese: cheese){
+           cheeses.remove(aCheese);
+       }
+        return  "redirect";
+    }
+
+
 }
